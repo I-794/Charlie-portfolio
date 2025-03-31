ebc47c82df3997c114c304732d94e0e3381945f2
Charlie <147570875+I-794@users.noreply.github.com>
2025-03-31 09:47:51 -0500

    Update App.jsx
    updated socials

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Charlie Lichterman",
+  title: "Student",
+  email: "charlie.lichterman@franklinsabers.org",
+  gitHub: "I-794",
   instagram: "microsoft",
   linkedIn: "satyanadella",
   medium: "",
